ae4bbbfe6ba6d80a161c2d8ee84329b2d965660d
Jay Guillot <guillotjay@gmail.com>
2022-05-05 01:40:26 -0500

    updates to frontend

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,15 +1,15 @@
 const contractAddress = "0xf36C99d2595640140a4191F1cEa596aDc4ba7759"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Fuzzy Cacti Co: Prickle Pear Edition Presale!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'Thank you for choosing a membership with us! Wallets analytics done right. Giving back with a Purpose.';
+const h1_presale_coming_soon = "Pre-sale Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon = "Enjoy all the perks that come with being an early supporter. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
